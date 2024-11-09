b4d474689e09dd5c86aed57b383998a1751e0276
Angel <jangeelopez@gmail.com>
2024-11-08 19:11:25 -0600

    V2

--- src/app/services/vatsim.service.ts
+++ src/app/services/vatsim.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { getNumberOfCurrencyDigits } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VatsimService {
-  private apiUrl = 'http://localhost:3000/api/vatsim/online-members'; // Cambia a la URL de tu servidor
+  private apiUrl = 'http://localhost:3000/api/vatsim/online-members';
 
   constructor(private http: HttpClient) {}
 
@@ -14,5 +15,3 @@ export class VatsimService {
     return this.http.get<any>(this.apiUrl);
   }
 }
-
-
